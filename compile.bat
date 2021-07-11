c4e79269c4b5ddd3a4216431edf7d54d4a50a3fc
Malcolm Easton <eastonmalcolm@gmail.com>
2021-07-10 19:18:25 -0600

    clean up compiler

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-.\Compiler\Ahk2Exe.exe /in .\min-to-notification.ah2 /out .\min-to-notification.exe
+.\Compiler\Ahk2Exe.exe /in .\min-to-notification.ah2 /out .\out\start.exe /icon icon.ico /ahk ./Compiler/AutoHotkey.exe
